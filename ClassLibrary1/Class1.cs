758ea8d2d9c0981300e5f164fb36dc1802448bff
Tad <tad.bojanowski@liazon.com>
2017-08-08 15:34:30 -0400

    First Binary Paowershell module test

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic; 
+using System.Text; 
+using System.Management.Automation;
 
 namespace ClassLibrary1
 {
-    public class Class1
+    [Cmdlet(VerbsCommon.Get,"Salutaion")]
+    public class GetSalutation :PSCmdlet
     {
+        private string[] nameCollection;
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ValueFromPipeline = true, Position = 0, HelpMessage = "Name")]
+        [Alias("Person", "FirstName")]
+        public string[] Name
+        {
+            get { return nameCollection; }
+            set { nameCollection = value; }
+        }
+
+        protected override void ProcessRecord()
+        {
+            foreach (string name in nameCollection)
+            {
+                WriteVerbose("Creating Salutation for " + name);
+                string salutation = "Hello, " + name;
+                WriteObject(salutation);
+            }
+        }
     }
 }
